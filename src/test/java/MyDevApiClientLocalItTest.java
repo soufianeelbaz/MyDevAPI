f15fcba9a60042823bf41343517cf0621a2d1895
SOUFIANE EL BAZ <soufiane.el.baz1@ibm.com>
2020-12-08 20:07:16 +0100

    SEZ - Initialisation de la structure des tests de l'API.

--- src/test/java/MyDevApiClientLocalItTest.java
+++ src/test/java/MyDevApiClientLocalItTest.java
@@ -1,66 +1,79 @@
-import com.ibm.mydev.MyDevSpringApplication;
 import com.ibm.mydev.personaldata.infrasctructure.mydev.api.configuration.MyDevApiConfiguration;
 import com.ibm.mydev.personaldata.infrasctructure.mydev.api.connected.MyDevApiClient;
+import com.ibm.mydev.personaldata.infrasctructure.mydev.api.connected.token.MyDevTokenService;
+import com.ibm.mydev.personaldata.infrasctructure.mydev.api.dto.MyDevUserView;
+import com.ibm.mydev.personaldata.infrasctructure.mydev.api.dto.UserItem;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.client.MockRestServiceServer;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import java.util.ArrayList;
+import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@RestClientTest(MyDevApiClient.class)
-@SpringBootTest(classes = MyDevSpringApplication.class)
-@ComponentScan(basePackages = {
-        "com.ibm.mydev.personaldata.infrasctructure.mydev.api"
-})
-@ActiveProfiles("MYDEV_REAL")
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.any;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MyDevApiClientLocalItTest {
 
-    @Autowired
-    @Qualifier("MyDevRestTemplate")
-    private RestTemplate restTemplate;
+    @Mock
+    MyDevApiConfiguration apiConfiguration;
+
+    @Mock
+    RestTemplate restTemplate;
+
+    @Mock
+    MyDevTokenService tokenService;
 
     @InjectMocks
-    @Autowired
-    private MyDevApiClient sut;
-
-    private MockRestServiceServer mockServer;
+    MyDevApiClient sut = new MyDevApiClient();
 
     @Before
     public void setUp() {
-        mockServer = MockRestServiceServer.bindTo(restTemplate).build();
+        MockitoAnnotations.initMocks(this);
+        apiConfiguration.baseUrl = "https://hr-bnpparibas-stg.csod.com/";
+        apiConfiguration.users = "services/api/x/odata/api/views/vw_rpt_user";
     }
 
     @Test
     public void testGetUserData() {
 
-        mockServer
-                .expect(requestTo("https://hr-bnpparibas-stg.csod.com/services/api/x/odata/api/views/vw_rpt_user?$filter=user_ref%2520eq%2520'cesadmin'&$select=user_ref,user_id,user_language_id"))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess("cesadmin", MediaType.APPLICATION_JSON));
+        Mockito.when(tokenService.getAccessToken()).thenReturn("TEST_TOKEN");
 
-        String collaborator = sut.getUserData("cesadmin").getValue().get(0).getCollaborator();
-        mockServer.verify();
+        MyDevUserView mockedUserView = new MyDevUserView();
 
-        Assert.assertEquals("cesadmin", collaborator);
+        UserItem user = new UserItem();
+        user.setId(1);
+        user.setLanguageId(2);
+        user.setCollaborator("cesadmin");
 
+        List<UserItem> users = new ArrayList<>();
+        users.add(user);
+
+        mockedUserView.setValue(users);
+
+        Mockito.when(restTemplate.exchange(
+                eq("https://hr-bnpparibas-stg.csod.com/services/api/x/odata/api/views/vw_rpt_user?$filter=user_ref%20eq%20'cesadmin'&$select=user_ref,user_id,user_language_id"),
+                eq(HttpMethod.GET),
+                any(HttpEntity.class),
+                eq(MyDevUserView.class)))
+                .thenReturn(new ResponseEntity<>(mockedUserView, HttpStatus.OK));
+
+        MyDevUserView userView = sut.getUserData("cesadmin");
+
+        Assert.assertNotNull(userView);
+        Assert.assertNotNull(userView.getValue());
+        Assert.assertEquals(1, userView.getValue().size());
+        Assert.assertEquals(Integer.valueOf(1), userView.getValue().get(0).getId());
+        Assert.assertEquals(Integer.valueOf(2), userView.getValue().get(0).getLanguageId());
+        Assert.assertEquals("cesadmin", userView.getValue().get(0).getCollaborator());
     }
 }
